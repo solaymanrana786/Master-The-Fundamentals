372bf9e321a5663184f6acf80dbffa3b4254e530
Rana AITS <ranaaits@Tuhins-MacBook-Pro.local>
2019-12-12 11:18:16 +0600

    init function without parameter

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,37 @@
 import UIKit
 
-var str = "Hello, playground"
+class Person {
+    var name = ""
+    init() {
+        name = "Solayman_Rana"
+    }
+
+}
+ 
+class Employee: Person {
+    var salary = 0
+    var role = "iOS_Developer"
+    
+    func doWork() {
+        print("Hi my name is \(name) and I'm doing work")
+        salary += 1
+    }
+}
+ 
+class Manager: Employee {
+    var teamSize = 5
+    
+    override func doWork() {
+        super.doWork()
+        
+        print("I'm managing people")
+        salary += 2
+    }
+    
+    func firePeople() {
+        print("I'm firing people")
+    }
+}
+
+let myPerson = Person()
+print(myPerson.name)
