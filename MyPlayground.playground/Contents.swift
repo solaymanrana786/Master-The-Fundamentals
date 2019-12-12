1f4e7294251cfab1875f056284903f43390a19e0
Rana AITS <ranaaits@Tuhins-MacBook-Pro.local>
2019-12-12 11:58:56 +0600

    init

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,1 +1,51 @@
 import UIKit
+
+
+class Person {
+    var name = ""
+    
+    init() {
+
+    }
+    
+    init(_ name: String) {
+        self.name = name
+    }
+
+}
+ 
+class Employee: Person {
+    var salary = 0
+    var role = ""
+    override init(_ name: String){
+        super.init(name)
+        self.role = "iOS_Developer"
+    }
+    func doWork() {
+        print("Hi my name is \(name) and I'm doing work")
+        salary += 1
+    }
+}
+ 
+class Manager: Employee {
+    var teamSize = 5
+    
+    override func doWork() {
+        super.doWork()
+        
+        print("I'm managing people")
+        salary += 2
+    }
+    
+    func firePeople() {
+        print("I'm firing people")
+    }
+}
+
+//let myPerson = Person("Solayman Rana")
+//print(myPerson.name)
+
+let anotherEmployee = Employee("Solayman Rana")
+print(anotherEmployee.name)
+print(anotherEmployee.role)
+
